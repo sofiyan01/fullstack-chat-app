37da00f0468e558188ada519d91408afbd134e50
Sayed Sofiyan <sofiyan9545@gmail.com>
2024-11-28 15:14:01 +0530

    Login state updated

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -9,7 +9,7 @@ export const useAuthStore = create((set, get) => ({
   // in starting state of user will be null
   authUser: null,
   isSigningUp: false,
-  isLoggingIng: false,
+  isLoggingIn: false,
   isUpdatingProfile: false,
   isCheckingAuth: true,
   onlineUsers: [],
@@ -44,7 +44,7 @@ export const useAuthStore = create((set, get) => ({
   },
 
   login: async (data) => {
-    set({ isLoggingIng: true });
+    set({ isLoggingIn: true });
     try {
       const res = await axiosInstance.post("/auth/login", data);
       set({ authUser: res.data });
@@ -54,7 +54,7 @@ export const useAuthStore = create((set, get) => ({
       toast.error(error.response.data.message);
       console.log("error in login", error);
     } finally {
-      set({ isLoggingIng: false });
+      set({ isLoggingIn: false });
     }
   },
 
